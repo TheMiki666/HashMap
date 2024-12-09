d6bb7816eef6e6254c9210b90eb5afff7dc03127
TheMiki666 <178334893+TheMiki666@users.noreply.github.com>
2024-12-09 19:32:37 +0100

    DO THE SET METHOD
    Done; except for the change of capacity

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -2,10 +2,43 @@ class HashMap
   PRIME_NUMBER = 31
   DEFAULT_CAPACITY = 16
   DEFAULT_LOAD_FACTOR = 0.75
+  
+  attr_reader :total_entries
+
+  #TODO: DELETE THIS
+  attr_reader :buckets
+
+  class Node
+    attr_reader :key, :value
+    attr_accessor :next_node
+
+    def initialize (key, value)
+      @key = key
+      @value = value
+      @next_node = nil
+    end
+
+    #returns true if the key is new (so the HasMap must increment the total entries by 1)
+    def append(new_node)
+      if new_node.key == @key
+        #replace value
+        @value = new_node.value
+        false
+      elsif @next_node.nil?
+        @next_node=new_node
+        true
+      else
+        @next_node.append(new_node)
+      end
+    end
+
+  end # of class Node
 
   def initialize (cap = DEFAULT_CAPACITY, lf = DEFAULT_LOAD_FACTOR)
     @capacity = cap
     @load_factor = lf
+    @buckets = Array.new(cap, nil)
+    @total_entries = 0
   end
 
   def hash(key)
@@ -13,4 +46,32 @@ class HashMap
     key.each_char {|char| hash_code = hash_code * PRIME_NUMBER + char.ord}
     hash_code
   end
+
+  def set(key, value)
+    new_node = Node.new(key, value)
+    index = hash(key) % @capacity
+    if @buckets[index].nil?
+      @total_entries += 1
+      @buckets[index] = new_node
+      change_capacity if time_to_change_capacity?
+    else
+      #Collision
+      if @buckets[index].append(new_node)
+        @total_entries += 1
+        change_capacity if time_to_change_capacity?
+      end
+    end
+  end
+
+  private
+
+  def time_to_change_capacity?
+    #Implement
+    return false
+  end
+
+  def change_capacity
+    # Empty for the moment
+  end
+
 end
--- main.rb
+++ main.rb
@@ -2,8 +2,16 @@ require_relative "lib/hash_map"
 
 map = HashMap.new
 
-puts map.hash("Thor")
-puts map.hash("Thor")
-puts map.hash("Merlin")
-puts map.hash("Thyra")
-puts map.hash("Questor")
+puts map.total_entries
+map.set("Erik", "Magneto")
+map.set("Bruce", "Batman")
+map.set("Clark", "Superman")
+map.set("Logan", "Wolverine")
+map.set("Ororo", "Storm")
+map.set("Steve", "Capitan America")
+map.set("Selina", "Catwoman")
+map.set("Diana", "Wonder Woman")
+map.set("Tony", "Ironman")
+puts map.total_entries
+p map.buckets
+
